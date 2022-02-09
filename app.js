cebcba111a0cc9d228c9d2cbc535e001ca0ca80d
bryantb148 <90650979+bryantb148@users.noreply.github.com>
2022-02-09 14:58:30 -0700

    yoyoyo

---
+++ app.js
@@ -0,0 +1,16 @@
+// set inital count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelctor("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn){
+    btn.addEventlistener("click", function (e){
+     const styles=e.currenttarget.classList      
+   
+     });
+});
+    
+    
+           
