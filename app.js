7eb018e75e9dd55b148c6c4ff61982269265c71e
bryantb148 <90650979+bryantb148@users.noreply.github.com>
2022-02-11 14:13:49 -0700

    jose

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn){
     btn.addEventlistener("click", function (e){
-     const styles=e.currenttarget.classList      
-   
+     const styles=e.currenttarget.classList;      
+  if(styles.contains('decrease')){
+     count--; 
+  }
+else if (styles.contains('increasing')){
+ count++; 
+}
+    else{
+  count = 0;
+    }
+     if(count> 0){
+         value.style.color = 'green';
+     }
+ if(count < 0){
+    value.style.color = 'red' 
+ }      
+  if(count=== < 0){
+      value.style.color ='#222;'
+  }       
+    value.textContent = count;
      });
 });
     
